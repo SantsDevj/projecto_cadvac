bda41910b867e148dcd71053c7e226cecd29b9f8
Santos Eduardo Júnior <santoseduardojuniors01@gmail.com>
2025-10-03 13:55:13 +0200

    commit changes #1

--- cadvac_sistema_de_cadastro_de_vacinas/src/dao_cadvac/UsuarioDAO.java
+++ cadvac_sistema_de_cadastro_de_vacinas/src/dao_cadvac/UsuarioDAO.java
@@ -2,7 +2,5 @@ package dao_cadvac;
 import back_implementation.*;
 
 public class UsuarioDAO extends BaseDAO{
-    public Usuario buscarPorEmailESenha(String enail, String senha){
-        return null;
-    }
+   
 }
